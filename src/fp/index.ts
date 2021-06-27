99584dc83a455aca44450a04c44156b059d83086
romainprignon <pro.rprignon@gmail.com>
2021-06-27 17:34:23 +0200

    🎨 better export as

--- src/fp/index.ts
+++ src/fp/index.ts
@@ -1,50 +1,10 @@
-import * as _array from './arrays'
-import * as _class from './classes'
-import * as _date from './dates'
-import * as _error from './errors'
-import * as _fs from './fs'
-import * as _function from './functions'
-import * as _number from './numbers'
-import * as _object from './objects'
-import * as _string from './strings'
-import * as _data from './data'
-
-export const arrays = {
-  ..._array
-}
-
-export const classes = {
-  ..._class
-}
-
-export const dates = {
-  ..._date
-}
-
-export const errors = {
-  ..._error
-}
-
-export const fs = {
-  ..._fs
-}
-
-export const functions = {
-  ..._function
-}
-
-export const numbers = {
-  ..._number
-}
-
-export const objects = {
-  ..._object
-}
-
-export const strings = {
-  ..._string
-}
-
-export const data = {
-  ..._data
-}
+export * as arrays from './arrays'
+export * as classes from './classes'
+export * as dates from './dates'
+export * as errors from './errors'
+export * as fs from './fs'
+export * as functions from './functions'
+export * as numbers from './numbers'
+export * as objects from './objects'
+export * as strings from './strings'
+export * as data from './data'
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,4 @@
-import * as _native from './native'
-import * as _fp from './fp'
-import * as _runtypes from './runtypes'
-import * as _zod from './zod'
-
-
-export const native = {
-  ..._native
-}
-
-export const fp = {
-  ..._fp
-}
-
-export const runtypes = {
-  ..._runtypes
-}
-
-export const zod = {
-  ..._zod
-}
+export * as native from './native'
+export * as fp from './fp'
+export * as runtypes from './runtypes'
+export * as zod from './zod'
