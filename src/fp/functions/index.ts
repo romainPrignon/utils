36c1370729157f9c4cea0c72441c1658255ef447
romainprignon <pro.rprignon@gmail.com>
2024-06-30 20:30:36 +0200

    ✨ Expose raise function

--- src/fp/functions/index.ts
+++ src/fp/functions/index.ts
@@ -1,3 +1,4 @@
 export * from './may'
 export * from './mayAsync'
 export * from './maybe'
+export * from './raise'
--- test/fp/functions/index.test.ts
+++ test/fp/functions/index.test.ts
@@ -1,20 +1,22 @@
 // test
-import { may, mayAsync, maybe } from '../../../src/fp/functions'
+import { may, mayAsync, maybe, raise } from '../../../src/fp/functions'
 import * as functions from '../../../src/fp/functions'
 
 
 describe('functions', () => {
   it('should export functions namespace', () => {
-    expect(Object.entries(functions).length).toEqual(3)
+    expect(Object.entries(functions).length).toEqual(4)
 
     expect(functions.may).toBeDefined()
     expect(functions.mayAsync).toBeDefined()
     expect(functions.maybe).toBeDefined()
+    expect(functions.raise).toBeDefined()
   })
 
   it('should export all namespace', () => {
     expect(may).toBeDefined()
     expect(mayAsync).toBeDefined()
     expect(maybe).toBeDefined()
+    expect(raise).toBeDefined()
   })
 })
